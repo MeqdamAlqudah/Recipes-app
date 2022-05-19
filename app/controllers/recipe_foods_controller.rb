81f1aa4b320a475d8396edc6fb6cba43fa714b30
Adorn Choga <adornchoga@gmail.com>
2022-05-19 13:30:33 +0200

    Fix recipe-food update bug

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,7 +1,7 @@
 class RecipeFoodsController < ApplicationController
   def update
     @recipe_food = RecipeFood.find(params[:id])
-    @recipe_food.update!(quantity: params[:quantity])
+    @recipe_food.update(quantity: params[:quantity])
     redirect_to recipe_path(@recipe_food.recipe_id)
   end
 
--- app/javascript/controllers/recipe_controller.js
+++ app/javascript/controllers/recipe_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.element.addEventListener('click', (e) => {
-      console.log('Clicked')
       const url = `http://127.0.0.1:3000/recipes/${e.target.dataset.recipeId}/recipe_status`
       fetch(url)
       window.location.reload()
