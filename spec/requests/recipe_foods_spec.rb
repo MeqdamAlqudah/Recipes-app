331c5576b453cc27968f61e830a06a82eada0711
Adorn Choga <adornchoga@gmail.com>
2022-05-19 12:05:10 +0200

    Add request specs for recipe_foods controller

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'RecipeFoods', type: :request do
-  # describe 'GET /destroy' do
-  #   it 'returns http success' do
-  #     get '/recipe_foods/destroy'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  let(:user) { create(:user) }
+  let(:food) { create(:food, user_id: user.id) }
+  let(:recipe) { create(:recipe, user_id: user.id) }
+  let(:recipe_food) { create(:recipe_food, food_id: food.id, recipe_id: recipe.id) }
+  before(:example) { login_as user }
+
+  describe 'GET /destroy' do
+    before(:example) { delete("/recipe_foods/#{recipe_food.id}") }
+    it 'returns http success' do
+      expect(response).to have_http_status(:found)
+    end
+  end
 end
