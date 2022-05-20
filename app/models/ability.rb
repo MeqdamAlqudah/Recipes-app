f0dc8d0d1de852fab8e29d3dcc2b74796868a8a7
Adorn Choga <adornchoga@gmail.com>
2022-05-20 17:54:51 +0200

    Fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,11 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-    can [:read, :create], Food
+    can %i[read create], Food
     can %i[read create], Recipe
     return unless user.present?
 
     can :destroy, Food, user: user
-    can :manage, Recipe, user: user
+    can :manage, Recipe, user:
   end
 end
